1f660f4e97c6c37123f08e36bb43e2ac6a6c1148
Andrea Ranzato <andrea.ranzato01@gmail.com>
2025-08-12 16:47:20 +0200

    feat: rename

--- R/tidy.R
+++ R/tidy.R
@@ -5,10 +5,12 @@ tidy <- function(dat) {
   validate(dat)
   colnames(dat) <- tolower(colnames(dat))
   colnames(dat)[colnames(dat) == "x"] <- "currency"
+  colnames(dat)[colnames(dat) == "descrizione"] <- "description"
+  colnames(dat)[colnames(dat) == "data"] <- "date"
 
   dat_sub <- dat[, setdiff(colnames(dat), "x.1")]
 
-  dat_sub[["data"]] <- as.Date(dat_sub[["data"]], format = "%d/%m/%Y")
+  dat_sub[["date"]] <- as.Date(dat_sub[["date"]], format = "%d/%m/%Y")
   dat_sub[["valuta"]] <- as.Date(dat_sub[["valuta"]], format = "%d/%m/%Y")
 
   # Clean DARE column: remove dots, replace comma with dot
@@ -41,11 +43,11 @@ tidy <- function(dat) {
   }
 
   # Remove balance rows that are not actual transactions using regex
-  if ("descrizione.operazione" %in% colnames(dat_sub)) {
+  if ("descrizione" %in% colnames(dat_sub)) {
     balance_pattern <- "^(Saldo contabile|Saldo liquido|Disponibilit\u00e0 al|Saldo SBF per conti unici al|Saldo iniziale)"
     exclude_rows <- grepl(
       balance_pattern,
-      dat_sub[["descrizione.operazione"]],
+      dat_sub[["descrizione"]],
       perl = TRUE
     )
     dat_sub <- dat_sub[!exclude_rows, ]
