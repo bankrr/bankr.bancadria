e3e779d921c59a0ae23bf27ebfa13c57db292037
Andrea Ranzato <andrea.ranzato01@gmail.com>
2025-09-02 18:08:24 +0200

    feat: sample transaction file

--- tests/testthat/test-validate.R
+++ tests/testthat/test-validate.R
@@ -1,6 +1,6 @@
 test_that("validate() works", {
   dat <- read.csv2(
-    pkg_file("testdata", "lista_mov.csv")
+    pkg_file("testdata", "transactions.csv")
   )
   expect_no_error(validate(dat))
 })
